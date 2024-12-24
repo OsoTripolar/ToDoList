93b3e5c6ca75e27e592e198d39f1140e563b00ee
oseto <omar.trafalgar.2001@gmail.com>
2024-12-24 15:06:37 -0500

    prueba antes del descontro

--- app.js
+++ app.js
@@ -11,12 +11,8 @@ const botonNuevaTarea = document.getElementById('nueva-tarea')
 const listaTareas = document.querySelector('.conjunto-tareas')
 const borrarLista = document.getElementById('borrar-lista')
 
-const listaTareasPendientes = []
-const listaTareasCompletadas = []
-
-let auxIndexPendientes = 0;
-let auxIndexCompletadas = 0;
-
+const tareasPendientes = ["estoy pendiente"]
+const tareasHechas = []
 
 // CREO QUE YA ESTÁ BIEN
 botonNuevaTarea.addEventListener('click', ()=>{
@@ -24,7 +20,7 @@ botonNuevaTarea.addEventListener('click', ()=>{
     if (input.value !== ""){
 
 
-        listaTareasPendientes.push(input.value)
+        tareasPendientes.push(input.value)
 
         // falta ponerle el índice
 
@@ -45,13 +41,11 @@ input.addEventListener('keydown', (e)=>{
 
 // MODIFICAR
 borrarLista.addEventListener('click', ()=>{
-    // listaTareas.textContent = "";
-    // limpiarInput();
-    listaTareasPendientes.length = 0
-    listaTareasCompletadas.length = 0
+    tareasPendientes.length = 0
+    tareasHechas.length = 0
 
-    console.log(listaTareasCompletadas);
-    console.log(listaTareasPendientes);
+    console.log(tareasHechas);
+    console.log(tareasPendientes);
     
     imprimirListas();
 })
@@ -66,16 +60,14 @@ function limpiarInput(){
 
 function imprimirListas(){
 
-    console.log("Pendientes");
-    console.log(listaTareasPendientes);
-    console.log("Completadas");
-    console.log(listaTareasCompletadas);
+    let auxIndexPendientes = 0;
+    let auxIndexCompletadas = 0;
 
     // limpiar capo
     listaTareas.textContent = "";
 
     // imprimir completadas
-    for(const pendientes of listaTareasCompletadas){
+    for(const pendientes of tareasHechas){
 
         const plantilla = document.querySelector('template').content.cloneNode(true)
         const p = plantilla.firstElementChild.firstElementChild // etiqueta p
@@ -94,7 +86,7 @@ function imprimirListas(){
         // Botón Borrar
         botonBorrar.addEventListener('click', (e)=>{
             const indiceActual = e.target.parentElement.parentElement.getAttribute('data-valor')
-            listaTareasCompletadas.splice(indiceActual, 1);
+            tareasHechas.splice(indiceActual, 1);
             imprimirListas();
         })
 
@@ -102,8 +94,8 @@ function imprimirListas(){
         botonListar.addEventListener('click', (e)=>{
 
             const indiceActual = e.target.parentElement.parentElement.getAttribute('data-valor')
-            listaTareasPendientes.push(listaTareasCompletadas[indiceActual])
-            listaTareasCompletadas.splice(indiceActual, 1);
+            tareasPendientes.push(tareasHechas[indiceActual])
+            tareasHechas.splice(indiceActual, 1);
             imprimirListas();
 
         })
@@ -118,7 +110,7 @@ function imprimirListas(){
 
     
     // imprimir pendientes
-    for(const pendientes of listaTareasPendientes){
+    for(const pendientes of tareasPendientes){
 
         const plantilla = document.querySelector('template').content.cloneNode(true)
         const p = plantilla.firstElementChild.firstElementChild // etiqueta p
@@ -133,7 +125,7 @@ function imprimirListas(){
         // Botón Borrar
         botonBorrar.addEventListener('click', (e)=>{
             const indiceActual = e.target.parentElement.parentElement.getAttribute('data-valor')
-            listaTareasPendientes.splice(indiceActual, 1);
+            tareasPendientes.splice(indiceActual, 1);
             imprimirListas();
         
             
@@ -143,8 +135,8 @@ function imprimirListas(){
         botonListar.addEventListener('click', (e)=>{
 
             const indiceActual = e.target.parentElement.parentElement.getAttribute('data-valor') // obteniendo el indice
-            listaTareasCompletadas.push(listaTareasPendientes[indiceActual]) // Añadiendo el valor actual a la otra array
-            listaTareasPendientes.splice(indiceActual, 1);
+            tareasHechas.push(tareasPendientes[indiceActual]) // Añadiendo el valor actual a la otra array
+            tareasPendientes.splice(indiceActual, 1);
             imprimirListas();
 
         })
@@ -154,6 +146,65 @@ function imprimirListas(){
         auxIndexPendientes++;
     }
 
-    auxIndexCompletadas = 0;
-    auxIndexPendientes = 0;
+}
+
+function verListasEnConsola(){
+    console.log("Pendientes");
+    console.log(tareasPendientes);
+    console.log("Completadas");
+    console.log(tareasHechas);
+}
+
+function pintaArrays(arrayOrigen, arrayHermano, isThisAListadeArray){
+
+    let auxIndice = 0;
+
+    for(const tarea of arrayOrigen){
+
+        verListasEnConsola();
+
+        // Acceso y clonación del template
+        const template = document.querySelector('template')
+        const clone = document.importNode(template.content, true);
+
+        // Seleccion de los elementos
+        const divTarea = clone.querySelector('.tarea-botones');
+        const p = clone.querySelector('p');
+        const botonBorrar = clone.querySelector('.boton-eliminar');
+        const botonListar = clone.querySelector('.boton-listar');
+        divTarea.setAttribute('data-valor', auxIndice);
+        p.textContent = tarea;
+
+        // Dependiendo del array aplicamos clases
+        if(isThisAListadeArray){
+            divTarea.classList.add('listado')
+        }
+
+        // Boton Borrar
+        botonBorrar.addEventListener('click', (e)=>{
+            const divTareaActual = e.target.parentElement.parentElement
+            const indexRef = divTareaActual.getAttribute.getAttribute('data-valor')
+            
+            arrayOrigen.splice(indexRef,1)
+        })
+        
+        // Boton Listar
+        botonListar.addEventListener('click', (e)=>{
+            const divTareaActual = e.target.parentElement.parentElement
+            const indexRef = divTareaActual.getAttribute.getAttribute('data-valor')
+
+            arrayHermano.push(arrayOrigen[indexRef]);
+            arrayOrigen.splice(indexRef, 1);
+
+            verListasEnConsola();
+        })
+
+        auxIndice++;
+        
+        // Vuelve a Imprimir en Pantalla
+        listaTareas.appendChild(divTarea);
+
+    }
+
+    verListasEnConsola();
 }
